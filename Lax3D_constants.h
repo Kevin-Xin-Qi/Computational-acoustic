a4280c941b6c34a8171768a082e4d9b5fe654166
Kevin-Xin-Qi <82510768+Kevin-Xin-Qi@users.noreply.github.com>
2021-04-14 11:51:37 +0100

    Add files via upload

---
+++ Lax3D_constants.h
@@ -0,0 +1,13 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define X 170
+#define Y 170
+#define Z 50
+#define T 50
+#define M_PIl 3.141592653589793238462643383279502884L /* pi */
+#define grid_size_x 0.01
+#define grid_size_y 0.01
+#define step_ratio 0.25
+#define c0 340
+
+#pragma once
