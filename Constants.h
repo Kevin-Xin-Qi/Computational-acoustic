e2cfb77c48689ac246f9f987a4ebf2d4f01e9d03
Kevin-Xin-Qi <82510768+Kevin-Xin-Qi@users.noreply.github.com>
2021-04-15 08:57:04 +0100

    Add files via upload

---
+++ Constants.h
@@ -0,0 +1,19 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define X 170
+#define Y 170
+#define Z 50
+#define T 500
+#define M_PIl 3.141592653589793238462643383279502884L /* pi */
+#define grid_size_x 0.01
+#define grid_size_y 0.01
+#define step_ratio 0.25
+#define c0 340
+
+
+
+
+
+
+
+
