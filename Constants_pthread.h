d2ffc9d3d8f52940f98cdb7c596386637fce7bad
Kevin-Xin-Qi <82510768+Kevin-Xin-Qi@users.noreply.github.com>
2021-04-19 17:51:52 +0100

    Add files via upload

---
+++ Constants_pthread.h
@@ -0,0 +1,22 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#define T 2
+#define M_PIl 3.141592653589793238462643383279502884L /* pi */
+#define grid_size_x 0.01
+#define grid_size_y 0.01
+#define step_ratio 0.25
+#define c0 340
+#define N   1
+#define AVE 17*2/N
+#define X AVE*N+1
+#define Y AVE*N+1
+#define Z 50
+
+
+
+
+
+
+
+
