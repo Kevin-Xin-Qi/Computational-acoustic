17c9ae0e79b0317e3754ac1079864912242f06c4
Kevin-Xin-Qi <82510768+Kevin-Xin-Qi@users.noreply.github.com>
2021-05-03 11:31:57 +0100

    Add files via upload

--- Lax_wendroff_3threads.c
+++ Lax_wendroff_3threads.c
@@ -2,9 +2,14 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
-#include"Constants_pthread.h"
+#define _CRT_SECURE_NO_WARNINGS
+#define T 500
+#define M_PIl 3.141592653589793238462643383279502884L /* pi */
+#define grid_size_x 0.01
+#define grid_size_y 0.01
+#define step_ratio 0.34
+#define c0 340
 #include <stdint.h>
-
 #include<time.h>
 #include<pthread.h>
 //#include<iostream>
